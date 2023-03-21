298b169be40e29d024db505a5fe765caf2c4c8d4
Jackysyl <1919879307@qq.com>
2023-03-21 11:50:11 -0400

    These are comments made by Yaolu Shen.

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+# These are comments made by Yaolu Shen.
 def encode(num):
     list = str(num)
     encode_str = ''
     for i in list:
         new_list = int(i) + 3
-        encode_str += str(new_list)
+        encode_str += str((new_list) % 10)
     return encode_str
 
 
-if __name__ == "__main__":
+def decoder(num):
+    string2 = " "
+    for i in range(len(num)):
+        string2 += str((int(num[i]) - 3) % 10)
+    return string2
 
+if __name__ == "__main__":
+    encoded_password = None
     while True:
         print("Menu")
         print("-------------")
@@ -19,11 +26,10 @@ if __name__ == "__main__":
         option = int(input("Please enter an option:"))
         if option == 1:
             num = input("Please enter your password to encode:")
-            encode(num)
+            encoded_password = encode(num)
             print("Your password has been encoded and stored!")
-        # if option == 2:
-        #     decode()
-        #     print(f"The encoded password is {encode(num)}, and the original password is {num}.")
+        if option == 2:
+            print(f"The encoded password is {encoded_password}, and the original password is {decoder(encoded_password)}.")
         if option == 3:
             break
 
